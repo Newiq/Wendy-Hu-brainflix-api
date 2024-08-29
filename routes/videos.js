c549b9529f2d6bb5ab9dc76c7c3cf1887f19bdd9
Newiq <784751939@qq.com>
2024-08-28 23:13:57 -0400

    upload video

--- routes/videos.js
+++ routes/videos.js
@@ -14,6 +14,12 @@ const router = express.Router();
     return JSON.parse(data);
     };
 
+
+
+    const writeVideoData = (data) => {
+    fs.writeFileSync(videoDataPath, JSON.stringify(data, null, 2));
+    };
+
     router.get('/', (_req, res) => {
     const videos = readVideoData();
     res.json(videos);
@@ -28,6 +34,29 @@ const router = express.Router();
     res.status(404).json({ message: "Video not found" });
     }
     });
+
+    router.post('/upload', (req, res) => {
+        const videos = readVideoData();
+        
+        const newVideo = {
+          id: `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`, // consider using np?
+        title: req.body.title || "Untitled Video",
+        description: req.body.description || "No description provided.",
+        thumbnail: '../public/images/default-thumbnail.jpg',  //will fix that
+        views: "0",
+        likes: "0",
+        duration: "0:00",  
+        video: "",  
+        timestamp: Date.now(),
+        comments: []
+    };
+    
+    videos.push(newVideo); 
+    writeVideoData(videos);  
+    
+    res.status(201).json(newVideo);
+    });
+
 
 
 export default router;
